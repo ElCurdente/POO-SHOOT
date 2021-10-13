695166cc3789421a17ad33f7ee8d5bef057d21f9
merwanjdly <79838415+merwanjdly@users.noreply.github.com>
2021-10-13 17:01:51 +0200

    vie

--- index.js
+++ index.js
@@ -64,9 +64,19 @@ setInterval(function () {
     else {
         coordonnees = coordonnees.filter(chute_en_cours);
         update_DOM();
-        compteurvies--;
+        compteurvies--;  
     }
 
+    if (compteurvies == 2) {
+        d3.select(".vie3").remove();
+    }
+    else if (compteurvies == 1) {
+ d3.select(".vie2").remove()
+    }
+     else if (compteurvies == 0) {
+        d3.select(".vie1").remove();
+        setTimeout(()=>{alert("recommence sale shlag");window.location.reload()},0);
+    }
 }, 20);
 
 
@@ -77,9 +87,7 @@ setInterval(function () {
     update_DOM();
 }, 1500);
 
-if (compteurvies == 2) {
-    d3.select("vie3").remove;
-}
+
 
 //Souris
 let mainlayer = svg.append("g")
